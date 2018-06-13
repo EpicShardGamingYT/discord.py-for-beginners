85a483e3aa575dbe481c20807c706d948556a583
GeorgeCY2 <36137970+GeorgeCY2@users.noreply.github.com>
2018-06-13 13:55:30 +0300

    Update ubuntu-install.sh

--- Async/Installing/Linux/ubuntu-install.sh
+++ Async/Installing/Linux/ubuntu-install.sh
@@ -1,9 +1,9 @@
-echo make sure to enter password!
+echo "make sure to enter password!"
 sudo apt install python3
 sudo apt install python3-pip
 pip3 install discord.py
-echo Done installing python and discord.py!
-echo install a good installer? close terminal if no or if yes, press enter
+echo "Done installing python and discord.py!"
+echo "install a good installer? close terminal if no or if yes, press enter"
 read ENTER
 sudo apt install nano
-echo Done installing it!
+echo "Done installing it!"
