4f56f8f02c6ba98a7db7e20edb9ec1beb6127b28
GeorgeCY2 <36137970+GeorgeCY2@users.noreply.github.com>
2018-06-13 15:12:35 +0300

    edited an echo
    found out how to terminate a shell process, so i edited it

--- Rewrite/Installing/Linux/ubuntu-install.sh
+++ Rewrite/Installing/Linux/ubuntu-install.sh
@@ -3,7 +3,7 @@ sudo apt install python3
 sudo apt install python3-pip
 pip3 install -U https://github.com/Rapptz/discord.py/archive/rewrite.zip
 echo "Done installing python and discord.py!"
-echo "install a good terminal editor? close terminal if no or if yes, press enter"
+echo "install a good terminal editor? CTRL+X if no or if yes, press enter"
 read ENTER
 sudo apt install nano
 echo "Done installing it!"
