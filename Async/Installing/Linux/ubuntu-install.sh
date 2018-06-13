a0d957fa3dd2acc058b4fd387a28587b7a67a29d
GeorgeCY2 <36137970+GeorgeCY2@users.noreply.github.com>
2018-06-13 14:03:55 +0300

    mistake

--- Async/Installing/Linux/ubuntu-install.sh
+++ Async/Installing/Linux/ubuntu-install.sh
@@ -3,7 +3,7 @@ sudo apt install python3
 sudo apt install python3-pip
 pip3 install discord.py
 echo "Done installing python and discord.py!"
-echo "install a good installer? close terminal if no or if yes, press enter"
+echo "install a good terminal editor? close terminal if no or if yes, press enter"
 read ENTER
 sudo apt install nano
 echo "Done installing it!"
